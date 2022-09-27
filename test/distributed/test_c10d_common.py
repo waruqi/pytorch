d0df983e8df9970cd98dfc4da94d6f5e4123111a
Howard Huang <howardhuang@fb.com>
2022-09-27 14:27:18 -0700

    [5/N] [Dispatchable Collectives] Update send with CPU / CUDA implementations
    ghstack-source-id: 2a1c63299fc62cbafae23add58a47f1e1c50322a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83859

--- test/distributed/test_c10d_common.py
+++ test/distributed/test_c10d_common.py
@@ -1468,6 +1468,7 @@ class ProcessGroupWithDispatchedCollectivesTests(MultiProcessTestCase):
             store=store,
         )
         collectives_and_args = [
+            (dist.send, self.rank),
             (dist.broadcast, self.rank),
             (dist.all_reduce,)
         ]

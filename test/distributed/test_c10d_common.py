7837b00251b8d2a071b93d47054ee604e2d27fa3
Howard Huang <howardhuang@fb.com>
2022-09-27 14:27:19 -0700

    [7/N] [Dispatchable Collectives] Update reduce with CPU / CUDA implementations
    ghstack-source-id: 2aa53f4c4ec37979a8a1423b0ff58626241d2e69
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83916

--- test/distributed/test_c10d_common.py
+++ test/distributed/test_c10d_common.py
@@ -1470,6 +1470,7 @@ class ProcessGroupWithDispatchedCollectivesTests(MultiProcessTestCase):
         collectives_and_args = [
             (dist.send, self.rank),
             (dist.recv, self.rank),
+            (dist.reduce, self.rank),
             (dist.broadcast, self.rank),
             (dist.all_reduce,)
         ]
